18871bf8679a547668ed8b2326492a6f553c8178
Alberto Cueto <cueto.alberto@gmail.com>
2017-01-30 17:58:23 -0600

    Adding the books application example from modern react with redux course

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+export default function() {
+  return [
+    { title: 'The blade itself' },
+    { title: 'Leviathan wakes' },
+    { title: 'The name of the wind' },
+    { title: 'The way of kings' },
+    { title: 'Wool' },
+    { title: 'Do androids dream of electric sheep?' },
+    { title: 'the shining' }
+  ]
+}
